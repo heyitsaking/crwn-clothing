fd1f96d4ce472bba1f157a5148d0ede33ca71631
heyitsaking <rey.arturo95@gmail.com>
2020-11-23 16:42:54 -0600

    Adding header component and links to home, shop page, and contact, also adding uniform formatting with prettier

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./collection-preview.styles.scss";
-import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
@@ -8,7 +8,7 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps}) => (
+        .map(({ id, ...otherItemProps }) => (
           <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
